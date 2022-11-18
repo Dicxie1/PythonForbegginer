812c90ea64f9ce3f84f3d299ed78db11d57c194a
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-18 15:14:29 -0600

    learning assiment operators

--- DataTypeAndOperators/pyprog04.py
+++ DataTypeAndOperators/pyprog04.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python3
 """
-@description example of use the comparation operators on python
+@description example of use the Assignment operators on python
 Symbol          mean
-(==)
-(!=)
-(<>)
-
+(=)             Assign            
+(+=)            add and assgins
+(*=)            multiply and assigns
+(/=)            divide and assigns
+(%=)            modulus and assigns
+(**=)           exponent and asssigns
+(//=)           Floor Division and assigns
 """
+
+# Variable x holds 10 and variable y holds 5
+x = 5
+y = 10
+
+x += y 
+print(f"Value of a post x += y is {x}")
+
+x *= y 
+print(f"Value of a post x *= y is {x}")
+
+x /= y 
+print(f"Value of a post x += y is {x}")
+
+x %= y 
+print(f"Value of a post x %= y is {x}")
+
+x **= y 
+print(f"Value of a post x **= y is {x}")
+
+x //= y 
+print(f"Value of a post x //= y is {x}")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PythonForBegginers 
 
+## Table Contents
+|#  | Subject / practice     | Source / File     |
+|---| :----------------------|-------------------|
+| 1 | Input sentences        | [pyprog01.py](DataTypeAndOperators/pyprog01.py)  |
+| 2 | Data types             | [pyprog02.py](DataTypeAndOperators/pyprog02.py)  |
+| 3 | Arithmetic Operators   | [pyprog03.py](DataTypeAndOperators/pyprog03.py)  |
+| 4 | Assignment Operators   | [pyprog04.py](DataTypeAndOperators/pyprog04.py)  |
