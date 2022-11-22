4bfd7a74676ab052522c0d88284aad5f29047328
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-22 09:57:06 -0600

    rename folder ControlStructure to ----> 02COntrolStruture

--- ControlStructure/pyprog01.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-var = -1
-if var < 0:
-    print(f"{var} the value of var is negative")
-#if there is only a single cluse the it may go on the same line as the header statement
-if var == -1: print("the value of var is negative")
-    
