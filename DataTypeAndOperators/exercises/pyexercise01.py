023605c288d050731d008ea4be52e66d1117032f
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-22 09:21:05 -0600

    data type and operators exersices

---
+++ DataTypeAndOperators/exercises/pyexercise01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Whrite a python program to calculate te length of a string 
+"""
+
+stringExample = "This is a string value" 
+
+print(f"The variable stringExample length is :{len(stringExample)}") # Display the length of stringExample
---
+++ DataTypeAndOperators/exercises/pyexercise02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Whrite a Python program to sum all the items in a list
+"""
+
+items =[10, 20, 2, 4]
+sum = 0
+for item in items:
+    sum += item
+print(f"the sum is: {sum}")
+
