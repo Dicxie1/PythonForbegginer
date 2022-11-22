928c267cb0ac4e6c4c94907e9f85d8d8c8152129
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-22 09:53:35 -0600

    example of while control struture in python

---
+++ ControlStructure/pyprog005.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+count = 0
+while (count < 3):
+    print(f"The count is: {count}")
+    count += 1
+else: # the else statement  execute when the condition is become false 
+    print(f"the loop end with: {count}")
+
