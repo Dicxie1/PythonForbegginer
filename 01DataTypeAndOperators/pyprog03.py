43f540d1cb1f356e86f0433afb869c754e95e4fe
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-22 09:56:16 -0600

    rename folder DataTypeAndOperators ---> 01DataTypeAndOperators

---
+++ 01DataTypeAndOperators/pyprog03.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+@description arithmetic operations on python
+
+the following it the avaible arithmetic operation on python
+Symbol      mean
+(+)         Addition
+(-)         Subtraction
+(*)         multiplication
+(/)         Division
+"""
+
+# variable y holds 10 and variable x hold 5
+x = 10
+y = 5
+
+# Addition
+print(f"Addition, x(10) + y(5) = {x + y}")
+
+# Subtraction
+print(f"Subtraction, x(10) - y(5) = {x - y}")
+
+# Multiplication
+print(f"Subtraction, x(10) * y(5) = {x * y}")
+
+# Division
+print(f"Division, x(10) / y(5) = {x / y}")
+
+# Module
+print(f"Modulus, x(10) % y(5) = {x % y}")
+
+# Exponent
+print(f"Exponent, x(10) ** y(5) = {x ** y}")
+
+# Floor Division
+print(f"Floor Division, x(10) // y(5) = {x // y}")
+
+
---
+++ 01DataTypeAndOperators/pyprog07.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#  Variable x holds 10 and variable y holds 5
+x = 10
+y = 5
+# Equal check operation
+print ("Equal check, x(10) == y(5) ", x == y)
+# Not Equal check operation
+print ("Not Equal check, x(10) != y(5) ", x != y)
+# Less than check operation
+print ("Less than check, x(10) <y(5) ", x<y)
+# Greater check operation
+print ("Greater than check, x(10) >y(5) ", x>y)
+# Less than or equal check operation
+print ("Less than or equal to check, x(10) <= y(5) ", x<= y)
+# Greater than or equal to check operation
+print ("Greater than or equal to check, x(10) >= y(5) ", x>= y)
