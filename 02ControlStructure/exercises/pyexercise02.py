8d3e2c8bb7345df066421a56e859f78d133d9920
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-23 12:37:19 -0600

    python program of mood computer

---
+++ 02ControlStructure/exercises/pyexercise02.py
@@ -0,0 +1,54 @@
+#¡/usr/bin/env python3
+# mood computer program
+from random import randint
+
+print("I sense your energy. Your true emotion are coming across my screen.")
+print("You are ....")
+
+mood = randint(1, 3) # generate random number between 1 to 3
+if mood == 1:
+    # happy
+    print(
+        """
+        ___________
+        |         |
+        | 0     0 |
+        |    <    |
+        |         |
+        | .     . |
+        | \´   \´ |
+        |   ...   |
+        |_________|
+        """)
+elif mood == 2:
+    # neutral
+    print(
+        """
+        ___________
+        |         |
+        | 0     0 |
+        |    <    |
+        |         |
+        |         |
+        | ....... |
+        |         |
+        |_________|  
+        """)
+elif mood == 3:
+    # sad
+    print(
+        """
+        ___________
+        |         |
+        | 0     0 |
+        |    <    |
+        |         |
+        |    .    |
+        |  ´   `  |
+        | ´    `  |
+        |_________|
+        """) 
+else: 
+    print("Illegal mood value! (you must be in a really bad mood)")
+print("....today")
+input("\n\nPress the Enter key to exit")
