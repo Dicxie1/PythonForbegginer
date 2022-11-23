92fe535857518ad959fea0c8891fd6021475d2ab
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-23 12:12:22 -0600

    pythom program simulate the login froma computer system

---
+++ 02ControlStructure/exercises/pyexercise01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Destrate of the if statement 
+# password program
+
+print("Welcome to System Security Inc.")
+print("-- where securirty is our middle name\n")
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+
+input("\n\nPress Enter key to exit...")
+
