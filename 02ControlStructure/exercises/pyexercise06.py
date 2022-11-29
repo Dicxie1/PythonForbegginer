fd652f5cf2e2289cdac883ae8cea9a88da01cd80
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-29 11:47:02 -0600

    Treating value as conditional

---
+++ 02ControlStructure/exercises/pyexercise06.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Application: pyexercise06.py
+Written by: Dicxie Madrigal <dicxiemadrigal@gmail.com>
+Purpose: Treating value as condional
+"""
+print("Welcome to Chateau D' Food")
+print("Is seems we are quite full ths evening.\n")
+money = int(input("How many dollars do you slip the Maitre D'"))
+if money:
+    print("Ah, I am reminded of a table. Right this way.")
+else:
+    print("Please, sit. It may be a while")
+
+input("\n\nPress the Enter key to exit.")
