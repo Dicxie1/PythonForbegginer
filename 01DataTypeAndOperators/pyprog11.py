189308ce17b63b391a766e26003af955849b9ce0
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-28 20:16:07 -0600

    example of escape sequences

---
+++ 01DataTypeAndOperators/pyprog11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Application: pyprog11.py
+Written by: Dicxie Madrigal <dicxiemadrigal@gmail.com>
+Purpose: Example of escape sequences 
+"""
+print("\t\t\t Fantacy Credits")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t by")
+print("\t\t\t Michael Dawson")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\nSpecial thanks goes out to:")
+print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
+input("\n\nPress the enter key to exit")
