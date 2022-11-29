c306df2535f9dc6c4bfb87ee78f6387a6017cfe2
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-28 19:59:37 -0600

    Delete the folder DataTypeAndOperators

--- DataTypeAndOperators/exercises/pyexercise04.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-"""
-@description a programa display a greatting to the user 
-"""
-name = input("What's your name?")
-print(f"Welcome {name} to programming with python from zero")
--- DataTypeAndOperators/pyprog09.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-var1 = "H"
-var2 = "Hello world!"
-
-print(var1 in var2)
-print("!" in var2)
