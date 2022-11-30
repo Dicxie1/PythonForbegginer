28622709cd9e121158a2924408d4b5fd2965f9a0
Dicxie Madrigal Brack <dicxiemadrigal@gmail.com>
2022-11-30 00:45:57 -0600

    Loop with a string

---
+++ 02ControlStructure/pyprog09.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Application:
+Written by: Dicxie Madrigal <dicxiemadrigal@gmail.com>
+Purpose: Loop with a string
+"""
+word = input("Enter the word: ")
+
+print("\nHere's each letter in your word:")
+for letter in word:
+    print(letter)
+
+print("\n\nPress Enter key to exit... ")
